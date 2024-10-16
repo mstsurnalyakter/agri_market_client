d05cb39fb873beead79b1e68ec594d87ec5697a6
mst-surnaly-akter <surnalyms@gmail.com>
2024-10-16 10:48:16 +0600

    material tailwind setup

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+import React from 'react'
+
+const NavBar = () => {
+
+    const pathName = usePathname();
+    const router = useRouter();
+    console.log(pathName);
+
+    const links = [
+        {
+            title:"Home",
+            path:"/"
+        },
+        {
+            title:"About",
+            path:"/about"
+        },
+        {
+            title:"Contact",
+            path:"/contact"
+        },
+        {
+            title:"Services",
+            path:"/services"
+        },
+        {
+            title:"Products",
+            path:"/product"
+        },
+        {
+            title:"Categories",
+            path:"/categories"
+        },
+        {
+            title:"Blogs",
+            path:"/blogs"
+        },
+        {
+            title:"Dashboard",
+            path:"/dashboard"
+        },
+    ]
+
+    const handleClick = () =>{
+        router.push('/about')
+    }
+
+    if (pathName.includes('dashboard')){
+        return <div className="bg-green-400 p-6 text-center">Dashboard Navbar</div>;
+    }
+
+  return (
+    <div className='flex bg-purple-400 items-center justify-between p-6'>
+        <h6>Next <span>App</span></h6>
+        <ul className='flex items-center justify-center gap-6'>
+           {
+            links.map(link=><Link className={`${pathName===link.path ? "text-teal-700 underline" : "text-indigo-100"} font-bold text-lg`} href={link.path} key={link.path}>{link.title}</Link>)
+           }
+        </ul>
+        <button onClick={handleClick} className='bg-green-600 rounded-md px-3 py-2'>Login</button>
+    </div>
+  )
+}
+
+export default NavBar
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,52 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+/** @type {import('tailwindcss').Config} */
+export default withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        poppins: ' "Poppins", sans-serif',
+      },
+      keyframes: {
+        animatedgradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        gradient: "animatedgradient 6s ease infinite alternate",
       },
     },
   },
   plugins: [],
-};
-export default config;
+});
